5631c35c66f47ccd942f34d1997e9d8cc38b9218
Mike Gardiner <mike@gardym.io>
2017-09-03 13:17:16 -0400

    Extract a Pages container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Page from './Page';
+import Pages from './Pages';
 import Inventory from './Inventory';
 import Interaction from './Interaction';
 import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
-    const pages = this.props.pages.map(p => {
-      return <Page page={p} />
-    });
-
     return (
       <div className="wrapper">
-        <div>
-          <div className="pages">
-            {pages}
-          </div>
-        </div>
+        <Pages />
         <Interaction />
         <Inventory />
       </div>
@@ -25,14 +16,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    pages: state.pages
-  }
-};
-
-const AppContainer = connect(
-  mapStateToProps
-)(App);
-
-export default AppContainer;
+export default App;
---
+++ src/Pages.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Page from './Page';
+import { connect } from 'react-redux';
+
+class Pages extends Component {
+  render() {
+    const pages = this.props.pages.map(p => {
+      return <Page page={p} />
+
+    });
+    return (
+        <div>
+          <div className="pages">
+            {pages}
+          </div>
+        </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    pages: state.pages
+  }
+};
+
+const PagesContainer = connect(
+  mapStateToProps
+)(Pages);
+
+export default PagesContainer;
