74ab4f9df5238742bbf3a5ac27cfa238985f3a90
Mike Gardiner <mike@gardym.io>
2017-09-05 11:31:17 -0500

    Switch statements are cool again

--- src/reducers/interaction.js
+++ src/reducers/interaction.js
@@ -1,20 +1,21 @@
 import interactions from './interactions';
 
 const interaction = (fullState, action) => {
-  if(action.type === "HOVER_INVENTORY_ITEM") {
-    return interactions.hoverInventoryItem(fullState, action);
-  } else if(action.type === "UNHOVER_INVENTORY_ITEM") {
-    return interactions.unhoverInventoryItem(fullState, action);
-  } else if(action.type === "USE_INVENTORY_ITEM") {
+  switch (action.type) {
+    case "HOVER_INVENTORY_ITEM":
+      return interactions.hoverInventoryItem(fullState, action);
+    case "UNHOVER_INVENTORY_ITEM":
+      return interactions.unhoverInventoryItem(fullState, action);
+    case "USE_INVENTORY_ITEM":
     return interactions.useInventoryItem(fullState, action);
-  } else if(action.type === "HOVER_HOTSPOT") {
-    return interactions.hoverHotspot(fullState, action);
-  } else if(action.type === "UNHOVER_HOTSPOT") {
-    return interactions.unhoverHotspot(fullState, action);
-  } else if(action.type === "USE_HOTSPOT") {
-    return interactions.useHotspot(fullState, action);
-  } else {
-    return fullState;
+    case "HOVER_HOTSPOT":
+      return interactions.hoverHotspot(fullState, action);
+    case "UNHOVER_HOTSPOT":
+      return interactions.unhoverHotspot(fullState, action);
+    case "USE_HOTSPOT":
+      return interactions.useHotspot(fullState, action);
+    default:
+      return fullState;
   }
 };
 
