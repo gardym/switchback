8d25fb35f6692118f39a38a1bee60b7f43553792
Mike Gardiner <mike@gardym.io>
2017-09-27 10:33:38 -0400

    Finish converting Act5 text, and build directly to the acts folder

--- parser/build-acts.js
+++ parser/build-acts.js
@@ -6,4 +6,4 @@ const act5Text = fs.readFileSync(__dirname + '/act5.act', 'utf8');
 
 const results = parsedActDecorator.parse(act5Text);
 
-fs.writeFileSync(__dirname + '/act5.js', JSON.stringify(results));
+fs.writeFileSync(__dirname + '/../src/acts/act5.json', JSON.stringify(results));
